1f80693c0f5541b29ce55350f6260e6b228c89f9
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-07-04 02:48:37 -0300

    room basic design

--- web/src/app/room/page.tsx
+++ web/src/app/room/page.tsx
@@ -1,18 +1,31 @@
-'use client'
-
-import { socket } from '@/libs/socket'
-import { useEffect, useState } from 'react'
+import { Scissors, Mountain, Scroll } from 'lucide-react'
 
 export default function Room() {
-  const [data, setData] = useState({})
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <div className="w-96 rounded bg-rosePineMoon-highlightMed p-6">
+        <strong className="flex items-center justify-center gap-2">
+          <h1 className="rounded bg-rosePineMoon-surface p-2 text-9xl text-rosePineMoon-iris">
+            0
+          </h1>
+          <span className="text-6xl">:</span>
+          <h1 className="rounded bg-rosePineMoon-surface p-2 text-9xl text-rosePineMoon-iris">
+            0
+          </h1>
+        </strong>
+      </div>
 
-  useEffect(() => {
-    socket.on('user entered', (data) => {
-      console.log(data)
-
-      setData(data)
-    })
-  }, [])
-
-  return <h1>{JSON.stringify(data, null, 2)}</h1>
+      <div className="flex gap-4">
+        <button className="rounded bg-rosePineMoon-rose p-4 text-3xl text-rosePineMoon-surface transition-opacity hover:opacity-70">
+          <Scissors />
+        </button>
+        <button className="rounded bg-rosePineMoon-rose p-4 text-3xl text-rosePineMoon-surface transition-opacity hover:opacity-70">
+          <Mountain />
+        </button>
+        <button className="rounded bg-rosePineMoon-rose p-4 text-3xl text-rosePineMoon-surface transition-opacity hover:opacity-70">
+          <Scroll />
+        </button>
+      </div>
+    </div>
+  )
 }
